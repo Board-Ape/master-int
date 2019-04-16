7a75da185ceda932d18c0c775a49b372959f42d2
Sam Singer <samuel.singer1@gmail.com>
2019-04-16 15:05:36 -0700

    Prepend

--- LinkedList.js
+++ LinkedList.js
@@ -10,7 +10,34 @@ class LinkedList {
         this.tail = this.head
         this.length = 1
     }
+
+    append(value) {
+        const newNode = {
+            value: value,
+            next: null
+        };
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+        return this;
+    }
+
+    prepend(value) {
+        const newNode = {
+            value: value,
+            next: null
+        }
+        newNode.next = this.head
+        this.head = newNode
+        this.length++
+        return this
+    }
 }
 
 const myLinkedList = new LinkedList(20)
 console.log(myLinkedList)
+myLinkedList.append(30)
+console.log(myLinkedList)
+myLinkedList.prepend(10);
+console.log(myLinkedList)
+
