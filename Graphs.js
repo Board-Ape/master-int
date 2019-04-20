1fd6d914f7f41f2ac88f8c2b3df0cd528d2536ff
Sam Singer <samuel.singer1@gmail.com>
2019-04-20 14:11:27 -0700

    Various ways to represent graphs

--- Graphs.js
+++ Graphs.js
@@ -12,3 +12,28 @@
         // Cyclic and Acyclic
             // Cyclic graphs are common in weighted graphs
 
+// Various ways to represent graphs
+
+//     2 - 0
+//    / \
+//   1 - 3
+
+// Edge List
+const graph = [[0,2],[2,3],[2,1],[1,3]];
+
+// Adjacency List
+const graph = [[2], [2,3], [0,1,3], [1,2]];
+
+// Adjacency Matrix
+const graphArray = [
+    [0,0,1,0],
+    [0,0,1,1],
+    [1,1,0,1],
+    [0,1,1,0]
+]
+const graphObject = {
+    0: [0,0,1,0],
+    1: [0,0,1,1],
+    2: [1,1,0,1],
+    3: [0,1,1,0]
+}
