45d5b222451c1a18aedea8f3abee8b6adb716708
Sam Singer <samuel.singer1@gmail.com>
2019-04-20 14:03:12 -0700

    Graphs overview

---
+++ Graphs.js
@@ -0,0 +1,14 @@
+// Graphs
+    // Internet is phenemonal example of a graph
+
+    // TYPES
+        // Directed and Undirected Graphs
+            // Facebook is similar to undirected
+            // Twitter is directed
+        // Weighted Graphs
+            // Values can assigend to the edges or other aspects.
+            // Optimal path execution
+            // Google maps use weighted graphs  
+        // Cyclic and Acyclic
+            // Cyclic graphs are common in weighted graphs
+
