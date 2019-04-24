8aac9299a9a4b519b8ab66a1ab1f5b6b64d71e20
Sam Singer <samuel.singer1@gmail.com>
2019-04-24 15:46:40 -0700

    Understading various sorting algos and when to implement

--- Sorting.js
+++ Sorting.js
@@ -106,3 +106,54 @@ function insertionSort(arr) {
 }
 
 console.log(insertionSort(array))
+
+
+// MERGE SORT
+
+// const ar = [2, 65, 34, 2, 1, 7, 8];
+
+// function mergeSort(arr) {
+//     if (arr.length === 1) {
+//         return arr
+//     }
+//     // Split array into right and left
+//     let midpoint = Math.floor(arr.legnth/2)
+//     let left = arr.slice(0,midpoint)
+//     let right = arr.slice(midpoint)
+
+//     return merge(mergeSort(left), mergeSort(right))
+// }
+
+// function merge(left, right) {
+//     const result = []
+//     let leftIdx = 0
+//     let rightIdx = 0
+//     while (leftIdx < left.length && rightIdx < right.length) {
+//         if (left[leftIdx] < right[rightIdx]) {
+//             result.push(left[leftIdx])
+//             leftIdx++
+//         } else {
+//             result.push(right[rightIdx])
+//             rightIdx++
+//         }
+//     }
+//     return result
+// }
+
+// console.log(mergeSort(ar))
+
+//#1 - Sort 10 schools around your house by distance:
+
+//#2 - eBay sorts listings by the current Bid amount:
+
+//#3 - Sport scores on ESPN
+
+//#4 - Massive database (can't fit all into memory) needs to sort through past year's user data
+
+//#5 - Almost sorted Udemy review data needs to update and add 2 new reviews
+
+//#6 - Temperature Records for the past 50 years in Canada
+
+//#7 - Large user name database needs to be sorted. Data is very random.
+
+//#8 - You want to teach sorting for the first time
