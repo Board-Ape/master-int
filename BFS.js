3de7984880959e12b15a520467223bcbdac9d688
Sam Singer <samuel.singer1@gmail.com>
2019-04-27 13:04:41 -0700

    Recursive solution

--- BFS.js
+++ BFS.js
@@ -56,11 +56,26 @@ class BinaryTree {
             if (currentNode.right) {
                 queue.push(currentNode.right)
             }
+        }
+        return result;
+    }
 
+    breadFirstSearchR(result, queue) {
+        if (!queue.length) {
+            return result
         }
 
-        return result;
+        let currentNode = queue.shift();
+        result.push(currentNode.value);
 
+        if (currentNode.left) {
+            queue.push(currentNode.left)
+        }
+        if (currentNode.right) {
+            queue.push(currentNode.right)
+        }
+
+        return this.breadFirstSearchR(result, queue)
     }
 }
 
@@ -82,3 +97,4 @@ myTree.insert(39);
 myTree.insert(50);
 console.log(JSON.stringify(traverse(myTree.root)));
 console.log(myTree.breadFirstSearch())
+console.log(myTree.breadFirstSearchR([], [myTree.root]));
